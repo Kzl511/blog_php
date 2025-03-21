95d5b67dc109d6d18e261d9260075a552a08f7bf
Kyaw Zayar Linn <kzl@Kyaws-MacBook-Air.local>
2025-03-22 00:14:20 +0800

    3rd Commit After Assignment

--- admin/header.php
+++ admin/header.php
@@ -107,6 +107,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
               </p>
             </a>
           </li>
+          <li class="nav-item">
+            <a href="register.php" class="nav-link">
+              <i class="nav-icon fas fa-user"></i>
+              <p>
+                Users
+              </p>
+            </a>
+          </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
---
+++ admin/register.php
@@ -0,0 +1,140 @@
+<?php
+
+session_start();
+require '../config/config.php';
+
+if ($_POST) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    if (!empty($_POST['isAdmin'])) {
+        $role = 1;
+        $stmt = $pdo->prepare("SELECT * FROM users WHERE email=:email");
+        $stmt->bindValue(':email', $email);
+        $stmt->execute();
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($user) {
+            echo "<script>alert('Email Duplicated!');</script>";
+        } else {
+            $stmt = $pdo->prepare("INSERT INTO users (name, email, password, role) VALUES(:name, :email, :password, :role)");
+            $result = $stmt->execute(
+                array(
+                    ':name' => $name,
+                    ':email' => $email,
+                    ':password' => $password,
+                    ':role' => $role
+                )
+            );
+
+            if ($result) {
+                echo "<script>alert('New Admin Account Created Successfully!');  window.location.href='index.php';</script>";
+            }
+        } 
+    } else {
+        $stmt = $pdo->prepare("SELECT * FROM users WHERE email=:email");
+        $stmt->bindValue(':email', $email);
+        $stmt->execute();
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($user) {
+            echo "<script>alert('Email Duplicated!');</script>";
+        } else {
+            $stmt = $pdo->prepare("INSERT INTO users (name, email, password) VALUES(:name, :email, :password)");
+            $result = $stmt->execute(
+                array(
+                    ':name' => $name,
+                    ':email' => $email,
+                    ':password' => $password,
+                )
+            );
+
+            if ($result) {
+                echo "<script>alert('New User Account Created Successfully!');  window.location.href='index.php';</script>";
+            }
+        }
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Blog | Login</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="../../index2.html"><b>Blog</b>Admin</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="card">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg">Register New Account</p>
+
+      <form action="register.php " method="post">
+      <div class="input-group mb-3">
+          <input type="text" name="name" class="form-control" placeholder="Name">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="email" name="email" class="form-control" placeholder="Email">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" name="password" class="form-control" placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+            <input type="checkbox" id="isAdmin" name="isAdmin" value="1" style="margin-right: 5px;">
+            <label for="isAdmin" class="mb-0"> Create as Admin?</label>
+        </div>
+        <div class="row">
+          <!-- /.col -->
+          <div class="container">
+            <button type="submit" class="btn btn-primary btn-block">Create Account</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+      <!-- <p class="mb-0">
+        <a href="register.html" class="text-center">Register a new membership</a>
+      </p> -->
+    </div>
+    <!-- /.login-card-body -->
+  </div>
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../dist/js/adminlte.min.js"></script>
+</body>
+</html>
